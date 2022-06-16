1bcc4caf2b482f431e8a49666b227179c183088c
Akqqa <alex@DESKTOP-E3D0H9G.localdomain>
2022-06-16 17:49:16 +0100

    Added draw and play methods to Game class

--- game/game.js
+++ game/game.js
@@ -10,6 +10,112 @@ class Game {
           }
         this.turn = 1
     }
+
+    // UNTESTED METHODS
+
+    // Method to draw from deck
+    // 0: Success
+    // 1: Invalid Player
+    // 2: No cards in deck
+    draw(playerID) {
+        var player;
+        // Checks if player is in the game
+        for (p of this.players) {
+            if (p.user == playerID) {
+                player = p;
+            }
+        }
+        
+        if (!player) {
+            return 1;
+        }
+
+        if (deck.length == 0) {
+            return 2;
+        }
+
+        // Moves top card from deck into hand
+        var card = player.deck.pop();
+        player.hand.push(card);
+        return 0;
+    }
+
+    // Method to play card to board
+    // 0: Success
+    // 1: Invalid player
+    // 2: Card not in hand
+    playToBoard(playerID, cardname, zone) {
+        var player;
+        // Checks if player is in the game
+        for (p of this.players) {
+            if (p.user == playerID) {
+                player = p;
+            }
+        }
+        
+        if (!player) {
+            return 1;
+        }
+
+        var selectedCard;
+        var index;
+        for (card in player.hand) {
+            if (cardname == card.name) {
+                selectedCard = card;
+                index = player.hand.indexOf(card);
+            }
+        }
+
+        // Error if card not in hand
+        if (!selectedCard) {
+            return 2;
+        }
+
+        // Removes card from hand
+        player.hand.splice(index, 1);
+        // Pushes card to board
+        this.board[zone].push(selectedCard);
+
+        return 0;
+    }
+
+    // Method to play spell card
+    // 0: Success
+    // 1: Invalid player
+    // 2: Card not in hand
+    playSpell(playerID, cardname, zone) {
+        var player;
+        // Checks if player is in the game
+        for (p of this.players) {
+            if (p.user == playerID) {
+                player = p;
+            }
+        }
+        
+        if (!player) {
+            return 1;
+        }
+
+        var selectedCard;
+        var index;
+        for (card in player.hand) {
+            if (cardname == card.name) {
+                selectedCard = card;
+                index = player.hand.indexOf(card);
+            }
+        }
+
+        // Error if card not in hand
+        if (!selectedCard) {
+            return 2;
+        }
+
+        // Removes card from hand
+        player.hand.splice(index, 1);
+        // Pushes card to grave
+        player.grave.push(selectedCard);
+        return card.cardInfo.image; // Returns image for bot to display (?)
+    }
 }
 
 class Player{
